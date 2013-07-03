b1f552fa938fc934766d1937720e3807a29d21bd
Nick Doiron <ndoiron@mapmeld.com>
2013-07-02 21:23:49 -0400

    add geojson and webgl examples

--- README.md
+++ README.md
@@ -20,13 +20,31 @@ and links.
 
 ### More Detailed Info
 
-* To change from data/data.csv to data/geojson.geojson or another file, add a tag just above the &lt;script&gt; tag for row-to-scope.js
+* To change from data/data.csv to data/data.geojson or another file, add a tag just above the &lt;script&gt; tag for row-to-scope.js
 
     &lt;script type="text/javascript"&gt;var dataSource = "data/geojson.geojson";&lt;/script&gt;
 
+* Put JavaScript code that runs after the correct row is loaded inside an init() function.
+
 * In JavaScript, replaceRow( "NAME" ) will convert a value in the first row to a value from the requested row
 
-* Put JavaScript code that runs after the correct row is loaded inside an init() function.
+* replaceRow also replaces numbers, arrays, and JSON objects from the first row.
+
+* jQuery functions $.html and $.text will automatically replace values from the first row.
+
+* For GeoJSON and future geo formats, put getGeometry() in place of GeoJSON. For example, with Leaflet:
+
+      L.geoJson( getGeometry() ).addTo(map);
+    
+or, to be more detailed and include replaceRow
+
+      L.geoJson( getGeometry(), {
+        onEachFeature: function(feature, layer){
+          layer.bindPopup( replaceRow("60647") );
+        }
+      }).addTo(map);
+
+* The previous(), next(), first(), and last() functions let you page through the rows.
 
 ### Tips
 
@@ -37,6 +55,8 @@ and links.
 * Fill the first row with column names like {{NAME}}, {{DESCRIPTION}}, {{HEIGHT}} so that it's easier to write your template page. /row?page=1 will be template, but the rest will have your data.
 
 * Put &lt;script&gt; and &lt;style&gt; tags in the &lt;head&gt; of the page.
+
+* (Future) If you have a meaningful ID for each row, use an {{ID}} column in a CSV, or ID property to a GeoJSON. Then link to pages using /row?id=SPECIAL_ID
 
 ## Supported Formats
 
@@ -49,7 +69,7 @@ and links.
 If you want to
 <a href="http://jekyllrb.com/">build blogs and no-CMS websites</a> or
 <a href="http://jlord.github.io/sheetsee.js/">visualize data from Google Spreadsheets</a>,
-there are other, much better tools to do that!
+there are much better tools to do that!
 
 Row-to-Scope is just designed to be as simple as possible.
 
--- javascript/row-to-scope.js
+++ javascript/row-to-scope.js
@@ -1,19 +1,48 @@
 // get CSV data
-$.get("../data/data.csv", function(data){
-  var rows = $.csv.toArrays(data);
+if(typeof dataSource == "undefined"){
+  dataSource = "../data/data.csv";
+}
+if(dataSource.toLowerCase().indexOf(".csv") > -1){
+  // CSV parser
+  $.get(dataSource, function(data){
+    var rows = $.csv.toArrays(data);
+    runRows(rows);
+  });
+}
+else{
+  $.getJSON(dataSource, function(data){
+    var myindex = 1 * (getURLVar("page") || getURLVar("row")) - 1;
+    getGeometry = function(){
+      return data.features[ myindex ];
+    };
+    runRows(data.features);
+  });
+}
 
+function runRows(rows){
   // get key and current page
   var keyrow = rows[ 0 ];
   var myrow = rows[ Math.max(0, 1 * (getURLVar("page") || getURLVar("row")) - 1) ];
   var mypageurl = (getURLVar("page") || getURLVar("row"));
   
-  for(var i=0;i<keyrow.length;i++){
-    document.title = replaceAll( document.title, keyrow[i], myrow[i] );
-    $(document.body).html( replaceAll( $(document.body).html(), keyrow[i], myrow[i] ) ); 
+  if(keyrow.length){
+    // CSV
+    for(var i=0;i<keyrow.length;i++){
+      document.title = replaceAll( document.title, keyrow[i], myrow[i] );
+      $(document.body).html( replaceAll( $(document.body).html(), keyrow[i], myrow[i] ) ); 
+    }
+  }
+  else if(typeof keyrow.properties != "undefined"){
+    // GeoJSON properties
+    for(var prop in keyrow.properties){
+      document.title = replaceAll( document.title, keyrow.properties[prop], myrow.properties[prop] );
+      $(document.body).html( replaceAll( $(document.body).html(), keyrow.properties[prop], myrow.properties[prop] ) ); 
+    }
   }
 
   // make page visible
   $(document.body).css({ visibility: "visible" });
+
 
   // replace jQuery html with one which replaces row
   Function.prototype.clone = function() {
@@ -41,14 +70,32 @@ $.get("../data/data.csv", function(data){
     }
     return $.fn.oldhtml.apply(this, arguments);
   };
-  
+  $.fn.oldtext = $.fn.text.clone();
+  $.fn.text = function(){
+    for(var a=0;a<arguments.length;a++){
+      for(var i=0;i<keyrow.length;i++){
+        arguments[a] = replaceAll( arguments[a], keyrow[i], myrow[i] ); 
+      }
+    }
+    return $.fn.oldtext.apply(this, arguments);
+  };
+
   // offer helper function to replace values with this row's values
   replaceRow = function(arg){
     if(typeof arg == "number"){
       // replace with fitting numbers
-      for(var i=0;i<keyrow.length;i++){
-        if(keyrow[i] * 1 === arg){
-          return myrow[i] * 1;
+      if(keyrow.length){
+        for(var i=0;i<keyrow.length;i++){
+          if(keyrow[i] * 1 === arg){
+            return myrow[i] * 1;
+          }
+        }
+      }
+      else if(typeof keyrow.properties != "undefined"){
+        for(var prop in keyrow.properties){
+          if(keyrow.properties[prop] * 1 === arg){
+            return myrow.properties[prop] * 1;
+          }
         }
       }
     }
@@ -61,20 +108,42 @@ $.get("../data/data.csv", function(data){
     }
     else if(typeof arg == "object"){
       // replace with fitting object
-      for(var i=0;i<keyrow.length;i++){
-        try{
-          if(JSON.parse(keyrow[i]) && JSON.parse(keyrow[i]) == arg){
-            return JSON.parse(myrow[i]);
+      if(keyrow.length){
+        for(var i=0;i<keyrow.length;i++){
+          try{
+            if(JSON.parse(keyrow[i]) && JSON.parse(keyrow[i]) == arg){
+              return JSON.parse(myrow[i]);
+            }
+          }
+          catch(e){
           }
         }
-        catch(e){
+      }
+      else if(typeof keyrow.properties != "undefined"){
+        for(var prop in keyrow.properties){
+          try{
+            if(JSON.parse(keyrow.properties[prop]) && JSON.parse(keyrow.properties[prop]) == arg){
+              return JSON.parse(myrow.properties[prop]);
+            }
+          }
+          catch(e){
+          }
         }
       }
     }
     else{
       // replace with fitting string
-      for(var i=0;i<keyrow.length;i++){
-        return replaceAll(arg, keyrow[i], myrow[i]);
+      if(keyrow.length){
+        for(var i=0;i<keyrow.length;i++){
+          arg = replaceAll(arg, keyrow[i], myrow[i]);
+        }
+        return arg;
+      }
+      else if(typeof keyrow.properties != "undefined"){
+        for(var prop in keyrow.properties){
+          arg = replaceAll(arg, keyrow.properties[prop], myrow.properties[prop]);
+        }
+        return arg;
       }
     }
   };
@@ -102,12 +171,15 @@ $.get("../data/data.csv", function(data){
   if(typeof init == "function"){
     init();
   }
-});
+}
 
 // placeholders for JS helper functions
-var replaceRow, next, previous, first, last;
+var replaceRow, next, previous, first, last, getGeometry;
 
 function replaceAll(src, oldr, newr){
+  if(typeof src == "undefined" || typeof src.indexOf == "undefined"){
+    return src;
+  }
   if(newr.indexOf(oldr) > -1){
     return src;
   }
